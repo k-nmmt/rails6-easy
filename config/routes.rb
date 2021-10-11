cbc3f70ecbd392f3ffd7e044a0bd0a56380843c6
k-nmmt <k.namimatsu@cybermissions.co.jp>
2021-10-11 18:57:42 +0900

    1011middle

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  get "/users/:id", to: "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'blogs#index'
   get "/blogs", to: "blogs#index"
